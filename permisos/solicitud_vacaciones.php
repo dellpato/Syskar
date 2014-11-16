85f48f46cf66ea0a20cbf611c5f0e1753828f9d5
Luis Cordón <r2lac.28@gmail.com>
2014-11-15 18:51:03 -0600

    Vacaciones

--- permisos/solicitud_vacaciones.php
+++ permisos/solicitud_vacaciones.php
@@ -3,19 +3,33 @@
  	
  	require_once("../class/class.permisos.php");
  	require('../fpdf/fpdf.php');
+	$hoy = getdate();
+	$dia = $hoy['mday'];
+	$mes = $hoy['mon'];
+	$annio = $hoy['year'];
 	
 	if(isset($_POST["btn_aceptar"])){
 		$Consulta = new realizar_consulta;
 		$Consulta->codigo = $_POST['txt_codigoempleado'];
 		$mostrar_empleado = $Consulta->consulta();
 		
-		foreach ($mostrar_empleado as $indice) {
-			$vnombre = $indice['Nombres'];
-			$vapellido = $indice['Apellidos'];
-			$vpuesto = $indice['Puesto'];
-			$vjefe = $indice['Jefe'];
-			$vdepto = $indice['Depto'];
-    	}
+		if(mostrar_empleado == ""){
+			foreach ($mostrar_empleado as $indice) {
+				$vnombre = $indice['Nombres'];
+				$vapellido = $indice['Apellidos'];
+				$vpuesto = $indice['Puesto'];
+				$vjefe = $indice['Jefe'];
+				$vdepto = $indice['Depto'];
+			}
+		}
+		else{
+			$vnombre="";
+			$vapellido="";
+			$vpuesto="";
+			$vjefe="";
+			$vdepto="";
+		
+		}
 		
 	
 	}//Fin del if de busqueda
@@ -55,32 +69,27 @@
 					</tr>
 					<tr>
 						<td><label>Nombres</label><br>
-							<input type= "text" name="txt_nombres" value="<?php echo $nombre; ?>" size=30></td>
+							<input type= "text" name="txt_nombres" value="<?php echo $vnombre; ?>" size=30></td>
 						<td><label>Apellidos</label><br>
-							<input type= "text" name="txt_apellidos" value="<?php echo $apellido;  ?>" size=30></td>
+							<input type= "text" name="txt_apellidos" value="<?php echo $vapellido;  ?>" size=30></td>
 					</tr>
 					<tr>
 						<td><label>Departamento Area</label><br>
-							<input type="text" name="txt_departamento" value="<?php echo $depto; ?>" size=30></td>
+							<input type="text" name="txt_departamento" value="<?php echo $vdepto; ?>" size=30></td>
 						<td><label>Cargo o Puesto</label><br>
-							<input type="text" name="txt_puesto" value="<?php echo $puesto; ?>" size=30></td>
+							<input type="text" name="txt_puesto" value="<?php echo $vpuesto; ?>" size=30></td>
 					</tr>
 					<tr>
 						<td colspan=2><label>Jefe Inmediato</label><br>
-							<input type="text" name="txt_jefeinmediato" value="<?php echo $jefe; ?>" size=64></td>
+							<input type="text" name="txt_jefeinmediato" value="<?php echo $vjefe; ?>" size=64></td>
 					</tr>
 					<tr>
-						<td><label>D&iacute;a de Permiso</label><br>
-							<input type="date" name="txt_fechapermiso"></td>
-						<td><label>Tipo de Permiso</label><br>
-							<select name="txt_tipovacaciones">
-								<option name="tipo_vacaciones1">Particular</option>
-								<option name="tipo_vacaciones2">Salud</option>
-							</select> </td>
+						<td><label>Fecha</label><br>
+							<input type="text" name="txt_fechapermiso" value="<?php echo "$dia/$mes/$annio"; ?>"></td>
 					</tr>
 					<tr>
-						<td></td>
 						<td><input type="submit" name="btn_guardar" value="Guardar">
+						<td></td>
 						</td>
 					</tr>
 				</table>
